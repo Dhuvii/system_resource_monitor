a7a5511eccc519c5b31695f406aa79c1db71d116
Dhuvii <thuvaraganuideisgns@gmail.com>
2023-03-18 22:15:57 +0530

    Complete IPC
    - Add all ipc functions to index

--- electron/ipc/index.ts
+++ electron/ipc/index.ts
@@ -1,1 +1,13 @@
-export function runIpcs() {}
+import cpuInfo from "./cpu.ipc";
+import memoryInfo from "./memory.ipc";
+import networkInfo from "./network.ipc";
+import batteryInfo from "./battery.ipc";
+import userInfo from "./user.ipc";
+
+export function runIpcs() {
+  cpuInfo();
+  memoryInfo();
+  networkInfo();
+  batteryInfo();
+  userInfo();
+}
